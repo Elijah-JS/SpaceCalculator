1c32d9413e053585045fe9b40db7ed46ee3bed44
Elijah Soghomonian <2002elijahjs@gmail.com>
2024-07-01 17:39:23 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [result,setResult] = useState("");
+
+  const ops = ['/','*', '+', '-','.']
+
+  const updateCalc = value=>{
+    setCalc(calc + value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='calculator'>
+        <div className='topbar'>
+          <input type='text' placeholder="0"/>
+        </div>
+        
+        <div className="button-row">
+          <button className="button" value="AC"> AC</button>
+          <button className="button" value="DE"> DE</button>
+          <button className="button" value="%"> %</button>
+          <button className="button" value="/"> /</button>
+        </div>
+        
+        <div className="button-row">
+          <button className="button" value="7"> 7</button>
+          <button className="button" value="8"> 8</button>
+          <button className="button" value="9"> 9</button>
+          <button className="button" value="x"> x</button>
+        </div>
+        
+        <div className="button-row">
+          <button className="button" value="4"> 4</button>
+          <button className="button" value="5"> 5</button>
+          <button className="button" value="6"> 6</button>
+          <button className="button" value="-"> -</button>
+        </div>
+        
+        <div className="button-row">
+          <button className="button" value="1"> 1</button>
+          <button className="button" value="2"> 2</button>
+          <button className="button" value="3"> 3</button>
+          <button className="button" value="+"> +</button>
+        </div>
+        
+        <div className="button-row">
+          <button className="button double-width" value="00"> 00</button>
+          <button className="button" value="0"> 0</button>
+          <button id= "yay"className="button equal-button" value="="> =</button>
+        </div>
+      </div>
     </div>
   );
 }
