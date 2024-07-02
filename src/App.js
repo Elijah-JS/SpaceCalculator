8910f7f420ebdd397d903e1e586e98f2926f5d33
Elijah Soghomonian <2002elijahjs@gmail.com>
2024-07-01 22:07:09 -0700

    Added sound affects

--- src/App.js
+++ src/App.js
@@ -3,20 +3,78 @@ import './App.css';
 
 function App() {
 
+   //functions to play click sound
+   const playClickSound = () =>{
+    const audio = new Audio('/pulse-blaster.mp3');
+    audio.volume = 0.12;
+    audio.play()
+  }
+  const playClickSound2 = () =>{
+    const audio = new Audio('/duh.mp3');
+    audio.volume = 0.75;
+    audio.play()
+  }
+  const playClickSound3 = () =>{
+    const audio = new Audio('/delete.mp3');
+    audio.volume = 0.75;
+    audio.play()
+  }
+  const playClickSound4 = () =>{
+    const audio = new Audio('/highspeed.mp3');
+    audio.volume = 0.75;
+    audio.play()
+  }
+  const playClickSound5 = () =>{
+    const audio = new Audio('/equal.mp3');
+    audio.volume = 0.75;
+    audio.play()
+  }
+
   const [calc, setCalc] = useState("");
   const [result,setResult] = useState("");
 
   const ops = ['/','*', '+', '-','.'];
-  const handlePercent=() => setCalc(calc/100);
-  const handleAllclear=() => setCalc("");
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+    const value = calc.slice(0,-1);
+    setCalc(value);
+    setResult(value);
+    playClickSound3();
+  }
+  const handlePercent=() =>{ 
+    setCalc(calc/100);
+    playClickSound4();
+  
+    }
+  const handleAllclear=() => {
+      setCalc("");
+      setResult("");
+      playClickSound2();
+  }
+  const calculate=() =>{
+     setCalc(eval(calc).toString())
+     playClickSound5();
+    };
+  
   const updateCalc = value=>{
     if (
       ops.includes(value) && calc === "" || 
       ops.includes(value) && ops.includes(calc.slice(-1))
-    ) {
+    )   {
+
       return;
-    }
+
+      }
     setCalc(calc + value);
+    if (!ops.includes(value)){
+      setResult(eval(calc+ value).toString());
+    }
+    playClickSound();
+
+   
+
   }
 //If result is truthy and calc is "5", {result ? <span>(0)</span> : ''} 
 //{calc || "0"} would render as (0)5.
@@ -26,13 +84,13 @@ function App() {
     <div className="container">
       <div className='calculator'>
         <div className='topbar'>
-        {result ? <span className='result'>(0)</span>: ''}
+        {result ? <span className='result'>({result})</span>: ''}
           {calc || "0"}  
                  </div>
         
         <div className="button-row">
           <button className="button" onClick={handleAllclear}>AC</button>
-          <button className="button" value="DE"> DE</button>
+          <button className="button" value="DE"onClick={deleteLast}> DE</button>
           <button className="button" value="%" onClick={handlePercent}> %</button>
           <button className="button" value="/"onClick={() => updateCalc('/')}> /</button>
         </div>
@@ -62,7 +120,7 @@ function App() {
           <button className="button " value="00"onClick={() => updateCalc('00')}> 00</button>
           <button className="button" value="0"onClick={() => updateCalc('0')}> 0</button>
           <button className="button " value="."onClick={() => updateCalc('.')}>.</button>
-          <button id= "yay"className="button equal-button" value="="> =</button>
+          <button id= "yay"className="button equal-button" value="="onClick={calculate}> =</button>
         </div>
       </div>
     </div>
